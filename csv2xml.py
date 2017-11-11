fe27adf6866d201f2d5487c7f5d035dde5af29c2
Jedsada Tiwongvorakul <jt@20scoops.net>
2017-11-11 23:12:04 +0700

    add feature arg string.xml (optional for use).

--- csv2xml.py
+++ csv2xml.py
@@ -8,11 +8,13 @@ import sys
 import os
 
 path_file_csv = raw_input("Path to CSV file : ")
-
 reader = csv.reader(open(path_file_csv, "rb"))
+
+xml_file_header = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n"
 translations = []
 roots = []
 current_path = os.getcwd()
+is_have_arg = False
 
 for row in reader:
     if row[0] == "key":
@@ -20,25 +22,33 @@ for row in reader:
         translations.pop(0)
         break
 
+if "arg" in translations : 
+    is_have_arg = True
+    translations.remove("arg")
+
 for translation in translations:
     roots.append(etree.Element("resources"))
 
-for row in reader:
-    if row[0] != "key":
-        number_of_translations = len(row) - 1
-        for i in range(0, number_of_translations):
+def set_name_value_and_comment(count_loop) :
+    for i in range(0, count_loop):
             if "#" in row[0]:
                 comment_message = row[0].replace(" ", "").split("#")[1]
                 comment = etree.Comment("=" * 15 + " "+comment_message + " " +"=" * 15)
                 roots[i].append(comment)
-            else :
+            elif len(str(row[0])) > 1:
+                string_result = row[i + 1].decode('utf-8')
+                result_arg = row[len(row) - 1].split("=")
+                arg_traget = "{"+result_arg[0]+"}"
+                if is_have_arg and arg_traget in string_result: 
+                    string_result = string_result.replace(arg_traget, result_arg[1])
                 string_resource = etree.SubElement(roots[i], "string")
                 string_resource.set("name", row[0])
-                string_resource.text = row[i + 1].decode('utf-8')
-                if number_of_translations == 1:
-                    string_resource.set("translatable", "false")
+                string_resource.text = string_result
 
-xml_file_header = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n"
+for row in reader:
+    if row[0] != "key":
+        number_of_translations = len(translations)
+        set_name_value_and_comment(number_of_translations)
 
 for i in range(0, len(translations)):
     android_dir = current_path + "/app/src/main/res/values"
