0ff6f5fd9bb651357d65c048f9f341e0430d1174
Jedsada Tiwongvorakul <jt@20scoops.net>
2017-11-11 16:21:56 +0700

    add commnet xml.

--- csv2xml.py
+++ csv2xml.py
@@ -15,7 +15,7 @@ roots = []
 current_path = os.getcwd()
 
 for row in reader:
-    if row[0] == "arbotena":
+    if row[0] == "key":
         translations = row
         translations.pop(0)
         break
@@ -23,16 +23,15 @@ for row in reader:
 for translation in translations:
     roots.append(etree.Element("resources"))
 
-translations.remove("_ios")
-translations.remove("_android")
-translations.remove("_sketch")
-translations.remove("detail")
-
 for row in reader:
-    if row[0] != "arbotena":
-        if row[0] != "General":
-            number_of_translations = len(row) - 1
-            for i in range(0, number_of_translations):
+    if row[0] != "key":
+        number_of_translations = len(row) - 1
+        for i in range(0, number_of_translations):
+            if "#" in row[0]:
+                comment_message = row[0].replace(" ", "").split("#")[1]
+                comment = etree.Comment("=" * 15 + " "+comment_message + " " +"=" * 15)
+                roots[i].append(comment)
+            else :
                 string_resource = etree.SubElement(roots[i], "string")
                 string_resource.set("name", row[0])
                 string_resource.text = row[i + 1].decode('utf-8')
@@ -46,7 +45,7 @@ for i in range(0, len(translations)):
     if translations[i] != "en" : 
         android_dir += "-"+translations[i].lower()
     if not os.path.exists(android_dir):
-        os.makedirs(android_dir)
+        os.makedirs(android_dir)    
     xml_string = etree.tostring(roots[i], encoding='unicode', method='xml', pretty_print=True, xml_declaration=False)
     pretty_xml_string = xml_file_header
     pretty_xml_string += xml_string 
